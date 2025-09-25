159defbd167d29d03a6f4c1bed810868d5bc7f2a
Jayson Ramirez <jramierz@brooklynsteamcenter.or>
2025-09-25 10:51:17 -0400

    Rootin Tootin Ranges Task 1 & 2 done

---
+++ 07_rootin_ranges.py
@@ -0,0 +1,66 @@
+"""
+Exercise 1:
+Write a program to print numbers from 1 to 10 using a for loop.
+"""
+print("------Exercise 1-------")
+print()
+print()
+for i in range(1, 11):
+    print(i)
+
+print("------Exercise 1-------")
+print()
+print()
+"""
+Exercise 2:
+Write a program to count by 10s from 900 to 1000
+"""
+print("------Exercise 2-------")
+print()
+print()
+for x in range (900, 1001, 10):
+    print(x)
+print("------Exercise 2-------")
+print()
+print()
+"""
+Exercise 3:
+Write a program that counts form 1-100 by 9
+"""
+print("------Exercise 3-------")
+print()
+print()
+
+print("------Exercise 3-------")
+print()
+print()
+"""
+Exercise 4:
+Write a program to calculate the sum of all numbers from 1 to 10 using a for loop.
+"""
+print("------Exercise 4-------")
+print()
+print()
+
+print("------Exercise 4-------")
+print()
+print()
+"""
+Excercise 5:
+Uncomment the following code and run it. Then answer the following:
+- What is the ouput of the code?
+
+- Explain the iterative process that this code executes to get that output
+"""
+print("------Exercise 5-------")
+print()
+print()
+# rows = 5
+# 
+# for i in range(rows):
+#     for j in range(i + 1):
+#         print('*', end=' ')
+#     print()
+print("------Exercise 5-------")
+print()
+print()
