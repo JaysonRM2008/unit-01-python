41fd3942d6634697ca870200c1b1efa6c3b95caa
Jayson Ramirez <jramierz@brooklynsteamcenter.or>
2025-10-01 09:27:42 -0400

    Updated

--- to_do.py
+++ to_do.py
@@ -1,49 +1,68 @@
-todos = [""] # Initial list of todos
-while True: #Main loop
-# Display current todo list
-    print("Your current to-dos are: ")
-# Use while loop to print each todo with a number (#)
+todos = []  # Start with an empty to-do list
 
-    i = 0
-    while i < len(todos):
-        print(f"#{i + 1} {todos[i]}")
-        i += 1
-# Show a message if the list is empty
-    if len(todos) == 0:
+while True:  # Main loop
+    # Display current todo list
+    if not todos:
         print("[No to-dos in your list]")
-        
+    else:
+        print("Your list contains:")
+        for i in range(len(todos)):
+            print(f"#{i + 1} {todos[i]}")
+
     # Separator line for clarity
     print("\n" + "~" * 50 + "\n")
 
- # Ask the user what they want to do
+    # Ask the user what they want to do
     action = input("Would you like to add, remove, edit, clear all, or exit? ").strip().lower()
 
-# Option to add a new todo
+    # Option to add a new todo
     if action == "add":
         new = input("What is your new to-do? ").strip()
         if new:
-            todos.append(new)  # Add to list
+            todos.append(new)
 
     # Option to remove a todo by number
     elif action == "remove":
         if todos:
-            try: 
-                num = int(input("Which # todo would you like to remove? "))
+            try:
+                num = int(input("Which # to-do would you like to remove? "))
                 if 1 <= num <= len(todos):
-                       todos.pop(num - 1)  # Remove the selected item
+                    removed = todos.pop(num - 1)
+                    print(f"Removed: {removed}")
                 else:
                     print("Invalid number.")
-            except:
+            except ValueError:
+                print("Please enter a valid number.")
+        else:
+            print("Nothing to remove.")
+
+    # Option to edit a to-do
+    elif action == "edit":
+        if todos:
+            try:
+                num = int(input("Which # to-do would you like to edit? "))
+                if 1 <= num <= len(todos):
+                    new_text = input("Enter the new text: ").strip()
+                    if new_text:
+                        todos[num - 1] = new_text
+                        print("To-do updated.")
+                else:
+                    print("Invalid number.")
+            except ValueError:
                 print("Please enter a valid number.")
         else:
             print("Nothing to edit.")
- # Option to clear the entire list
+
+    # Option to clear the entire list
     elif action == "clear all":
         todos.clear()
+        print("All to-dos have been cleared.")
 
- # Exit the program
+    # Exit the program
     elif action == "exit":
+        print("Goodbye!")
         break
+
     # Catch invalid inputs
     else:
         print("Invalid option.")
