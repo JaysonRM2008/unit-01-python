864dc1e70bef860acb7a94f42028bc35488240e3
Jayson Ramirez <jramierz@brooklynsteamcenter.or>
2025-10-06 09:48:40 -0400

    debugging task done

--- 08_debugging.py
+++ 08_debugging.py
@@ -31,3 +31,47 @@ for num in range(1, n + 1):  # Correctly loop up to and including the user's num
 print("------Code 3-------")
 print()
 print()
+
+print("------Code 4-------")
+print()
+print()
+num = int(input("Enter an integer: "))
+
+if num < 0:
+  print("Factorial does not exist for negative numbers.")
+elif num == 0:
+  print("The factorial of 0 is 1.")
+else:
+  result = 1
+  for i in range(1, num + 1):  # Correctly includes num in the loop
+    result *= i
+  print("Factorial of " + str(num) + " is " + str(result)) # Converts integers to strings for printing
+
+  print("------Code 4-------")
+print()
+print()
+
+print("------Code 5-------")
+print()
+print()
+attempts = 0
+correct_password = "secret"
+
+while True:
+    password = input("Enter your password: ")
+    attempts += 1
+
+    if password == correct_password:
+        print("Correct password!")
+        break  # End the loop if the password is correct
+    else:
+        print("Incorrect password")
+    
+    if attempts >= 3:
+        print("Too many attempts")
+        break  # End the loop after a maximum number of attempts
+
+print("------Code 5-------")
+print()
+print()
+
